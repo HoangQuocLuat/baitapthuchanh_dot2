09c37b1964689c71ca5ff14e44d7f9d343542db9
HoangQuocLuat <2021050769@student.humg.edu.vn>
2023-10-07 09:46:36 +0700

    mvc b1

---
+++ MVC_Project/Controllers/PersonController.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Project.Controllers
+{
+
+    public class PersonController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
+}
